7544ba190f7a33b6af89f6754fbac66c86af4705
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-19 07:28:24 -0300

    added fun getNextWorld()

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,17 +4,39 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
+    private var _score = 0
+    val score:Int get() =_score
+    private var currentWordCount = 0
+    private lateinit var _currentScrambledWord :String
+
+    private var wordsList : MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
     init {
         Log.d("GameFragment","GameViewModel created")
+        getNextWord()
     }
 
-    private var _score = 0
-    private var _currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private fun getNextWord() {
+        do{
+            currentWord = allWordsList.random()
+        }while (wordsList.contains(currentWord))
 
-    val score :Int get() = _score
-    val currentWordCount :Int get() = _currentWordCount
-    val currentScrambledWord:String get() = _currentScrambledWord
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+
+        _currentScrambledWord = String(tempWord)
+        ++currentWordCount
+        wordsList.add(currentWord)
+
+    }
 
     override fun onCleared() {
         super.onCleared()
